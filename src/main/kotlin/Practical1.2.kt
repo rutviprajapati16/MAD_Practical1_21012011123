c78b6fb70ed67ab583e6411bf95dc99073562a37
RUTVI <rutvi162004@gmail.com>
2023-07-19 21:35:57 +0530

    rp

---
+++ src/main/kotlin/Practical1.2.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var a : Int = 10
+    var new_a : Double = a.toDouble()
+    println("Integerr Value: $a \nDouble From Integer: $new_a")
+
+    var b : String = "10"
+    var new_b : Int = b.toInt()
+    var dou_b : Double = b.toDouble()
+    var new = dou_b+1.12
+    println("String Value: $b \nInteger Value1: $new_b \nInteger Value2: $new_b")
+    println("Double Value: $new")
+}
